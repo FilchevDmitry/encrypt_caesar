621e7da99ee4f6b1700fa7e827888d86e66bd944
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-02-27 09:02:48 +0300

    New

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+std::string encrypt_caesar(std::string text, int num) {
+    for (int i = 0; i < text.length(); i++) {
+        if(text[i]>=97 && text[i]<=122 ){
+            if(num>0)
+                (text[i] + num < 122) ? text[i] = text[i] + num : text[i] = 97 + ((text[i] + num) - 123);
+            else
+                (text[i] + num >= 97) ? text[i] = text[i] + num : text[i] = 123 + ((text[i] + num) - 97);
+        }
+        if(text[i]>=65 && text[i]<=90){
+            if(num>0)
+                (text[i] + num < 90) ? text[i] = text[i] + num:text[i] = 65 + ((text[i] + num) - 91);
+            else
+                (text[i] + num >= 65) ? text[i] = text[i] + num : text[i] = 91 + ((text[i] + num) - 65);
+        }
+    }
+    return text;
+}
+std::string decrypt_caesar(std::string text, int num){
+    num*=-1;
+    std::string text1=encrypt_caesar(text,num);
+    return text1;
+}
+
+
+int main() {
+    std::cout << "Encrypt_caesar\n";
+    std::string text;
+    std::string question;
+    int num = 0;
+    std::cout << "Input text :";
+    std::getline(std::cin, text);
+    std::cout << "Enter by what number you need to shift :" << std::endl;
+    std::cin >> num;
+    (num>0)? num=num%26 : num=num%-26;
+    bool ques=true;
+    do {
+        std::cout << "Need to encrypt or decrypt ?";
+        std::cin >> question;
+        if(question=="encrypt" ||question=="decrypt")
+            ques = false;
+    }
+    while(ques);
+
+    (question=="encrypt")? std::cout << encrypt_caesar(text, num) << std::endl :
+    std::cout <<decrypt_caesar(text,num)<<std::endl;
+}
